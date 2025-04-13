8eb535f92a3ba2d18120318d9b8d27472a59b33e
Andrii Levchenko <levchenkoandrii11@gmail.com>
2025-04-13 19:04:32 +0300

    Solution

--- install-app.sh
+++ install-app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Script to silently install and start the todo web app on the virtual machine. 
-# Note that all commands bellow are without sudo - that's because extention mechanism 
-# runs scripts under root user. 
+# Note that all commands bellow are without sudo - that's because extention mechanism
+# runs scripts under root user.
 
 # install system updates and isntall python3-pip package using apt. '-yq' flags are
 # used to suppress any interactive prompts - we won't be able to confirm operation
